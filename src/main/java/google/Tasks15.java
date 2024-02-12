503a777997d7102f863c9c040351b762853402ab
RAVIAVARMANV <ravieee2020@gmail.com>
2024-02-12 09:55:12 +0530

    Task15

---
+++ src/main/java/google/Tasks15.java
@@ -0,0 +1,13 @@
+package google;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Tasks15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://medium.com/@ravieee2020/task-15-7e91ee8d506c");
+	}
+
+}
---
+++ src/main/java/google/google.java
@@ -0,0 +1,13 @@
+package google;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class google {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.google.co.in/");
+	}
+
+}
